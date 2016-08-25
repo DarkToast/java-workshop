0bdc14e18dae7e04f6abb9df1c5d833fb4e00b98
Christian <christian.iwanzik@gmail.com>
2016-08-25 22:07:42 +0200

    Some basic stuff

--- basics/src/main/java/de/darktoast/Application.java
+++ basics/src/main/java/de/darktoast/Application.java
@@ -1,7 +1,31 @@
 package de.darktoast;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
 
+
+        Application application = new Application();
+        List<String> list = new ArrayList<>();
+        list.add("Foo");
+        list.add("Bar");
+        list.add("Baz");
+
+        List<String> unmodifiableList = Collections.unmodifiableList(list);
+
+        application.filter(list);
+    }
+
+
+    public List<String> filter(List<String> list) {
+        for(String name : list) {
+            System.out.println(name);
+            list.remove(name);
+        }
+
+        return list;
     }
 }
--- basics/src/test/groovy/de/darktoast/SquareSpec.groovy
+++ basics/src/test/groovy/de/darktoast/SquareSpec.groovy
@@ -5,16 +5,18 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 class SquareSpec extends Specification {
-
     def "A square's extensive is four times of its side"() {
         given: "A square with a side length of 4"
-        def square = new Square(4)
+        def square =  Mock(Square)
 
         when: "We calculate the extensive"
         double extensive = square.extensive()
 
         then: "We get the extensive amount of 16"
         extensive == 16.0D
+
+        and: "the mock was called one time"
+        1 * square.extensive() >> 16
     }
 
     @Unroll
