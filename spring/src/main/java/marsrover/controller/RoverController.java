80df9514a38bea5f01f5da2bf69d9cfa3ba7b7e1
Christian <christian.iwanzik@gmail.com>
2016-08-28 18:18:19 +0200

    Adding a fancy controller

--- spring/src/main/java/marsrover/controller/RoverController.java
+++ spring/src/main/java/marsrover/controller/RoverController.java
@@ -1,15 +1,58 @@
 package marsrover.controller;
 
+import marsrover.rover.Rover;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RoverController {
 
-    @RequestMapping(path = "/get", method = RequestMethod.GET)
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("foobar");
+    @Autowired
+    Rover rover;
+
+    @RequestMapping(path = "/rover", method = RequestMethod.GET)
+    public ResponseEntity<String> rover() {
+        final String position = rover.getActualPosition().toString();
+        final String direction = rover.getDirection().toString();
+        return ResponseEntity.ok(direction + " | " +position);
+    }
+
+    @RequestMapping(path = "/rover/position", method = RequestMethod.GET)
+    public ResponseEntity<String> position() {
+        return ResponseEntity.ok(rover.getActualPosition().toString());
+    }
+
+    @RequestMapping(path = "/rover/direction", method = RequestMethod.GET)
+    public ResponseEntity<String> direction() {
+        return ResponseEntity.ok(rover.getDirection().toString());
+    }
+
+    @RequestMapping(path = "/rover", method = RequestMethod.PUT)
+    public ResponseEntity<String> command(@RequestBody String command) {
+        for (char c : command.toCharArray()) {
+            switch (c) {
+                case 'F':
+                case 'f':
+                    rover.moveForward();
+                    break;
+                case 'B':
+                case 'b':
+                    rover.moveBackward();
+                    break;
+                case 'L':
+                case 'l':
+                    rover.turnLeft();
+                    break;
+                case 'R':
+                case 'r':
+                    rover.turnRight();
+            }
+        }
+
+        return rover();
     }
 }
